fdfa93b1da243ace22369143ec870576988fdae6
LAPTOP-N35QDRHV\solon <urielbarragan49@gmail.com>
2024-09-04 11:03:31 -0600

    se agregan los proyectos de negocio, infraestructura y en microservicio de choferes

---
+++ Gen9-Parent/Gen9-negocio/Gen9-choferes/src/main/java/com/barragan/choferes/Gen9ChoferesApplication.java
@@ -0,0 +1,13 @@
+package com.barragan.choferes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gen9ChoferesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gen9ChoferesApplication.class, args);
+	}
+
+}
